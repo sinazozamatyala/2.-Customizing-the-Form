4f4fc996d4498c2b26999c1d15af37168d140c99
Lenovo-User <Lenovo-User@DESKTOP-JS25F1I>
2024-01-18 21:32:29 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClose = New Button()
+        btnSinazo = New Button()
+        SuspendLayout()
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(249, 281)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(178, 63)
+        btnClose.TabIndex = 0
+        btnClose.Text = "Close"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' btnSinazo
+        ' 
+        btnSinazo.Location = New Point(231, 98)
+        btnSinazo.Name = "btnSinazo"
+        btnSinazo.Size = New Size(224, 54)
+        btnSinazo.TabIndex = 1
+        btnSinazo.Text = "Sinazo Zamatyala"
+        btnSinazo.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(128))
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnSinazo)
+        Controls.Add(btnClose)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnSinazo As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+    Private Sub btnSinazo_Click(sender As Object, e As EventArgs) Handles btnSinazo.Click
+        MsgBox("This is Sinazo Zamatyala", , "Greeting")
+    End Sub
+End Class
